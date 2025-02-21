ab8379cb5f320e7577c76ab0b2f8736f53e01964
micheledimassimo <mcldms@outlook.it>
2025-02-21 09:37:59 +0100

    responsive appheader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -11,7 +11,7 @@ export default {
 
 <template>
 	<header>
-		<div class="my-container text-light">
+		<div class="container-fluid text-light">
 			<!--navbar-->
 			<div class="container-lg ">
 				<nav>
@@ -22,7 +22,7 @@ export default {
 						<li>
 							<span class="fw-bold">IT </span> <span>/ EN</span>
 						</li>
-						<li>
+						<li class="d-none d-md-inline">
 							<a href="#">
 								<i class="fa-brands fa-twitter"></i>
 							</a>
@@ -39,12 +39,12 @@ export default {
 
 			<div class="container-sm">
 				<div class="row">
-					<div class="col-6">
+					<div class="col-lg-6 col-sm-12">
 						<p class="fw-bold">
 							Plans are nothing, planning is everything
 						</p>
 					</div>
-					<div class="col-6">
+					<div class="col-lg-6 col-sm-12">
 						<h6 class="text-warning">
 							Maria Rossi
 						</h6>
@@ -57,6 +57,20 @@ export default {
 						</a>
 					</div>
 				</div>
+				<div class="d-md-none py-4 ps-3">
+					<a href="#">
+						<i class="fa-brands fa-facebook"></i>
+					</a>
+					<a href="#">
+						<i class="fa-brands fa-twitter"></i>
+					</a>
+					<a href="#">
+						<i class="fa-brands fa-youtube"></i>
+							</a>
+					<a href="#">
+						<i class="fa-brands fa-linkedin"></i>
+					</a>
+				</div>
 			</div>
 		</div>
 	</header>
@@ -65,9 +79,9 @@ export default {
 <style lang="scss" scoped>
 @use '../assets/scss/partials/variables' as *;
 
-.my-container {
+.container-fluid {
 	background-image: url(https://s3-alpha-sig.figma.com/img/d7c5/7ec9/4aecaa048b29f54912029661e32e11c2?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=cMpY2SoiSyA~Tg-gIIn~Q79pGd7fNhRc9KcWtXBd9tiRcaz8n9NqO59VJ4jZybRAlYq~zmh9jLOFIGewuKDfol2mhe-lPjIjPX7hSs21gUTtSOkL96JjS0Rr~LCLuyEjBZ0z9tjc2SAYVVVyyru757G91VSF5YkBuV~6~faqQQDNl13OT2Ahx1~A9t6XzmDzVG8PBUb8RA-jmnMJm~utvNDo26V2DQ8st6gbc8jrp6JPQ6VyDcQxPmSy74-ZGvLPkK3jQ-LDw5TYr~iaCxOJyl5cB5NQL7aAjSNAtyTPfVI7Sc-~-e2~uWJmRO-cqgkg7sgUlFDuxbwtncx7JgVT9w__);
-	background-repeat: no-repeat;
+	
 	background-size: cover;
 	
 	.container-lg {
@@ -85,12 +99,12 @@ export default {
 					> span:nth-child(2n) {
 						opacity: 0.7;
 					}
-					> a {
+					/* > a {
 						text-decoration: none;
 						color: white;
 						padding-left: 15px;
 						font-size: 1.5rem;
-					}
+					} */
 				}
 				
 			}
@@ -99,9 +113,9 @@ export default {
 	.container-sm {
 		.row {
 			align-items: flex-start;
-			padding: 100px;
+			padding-bottom: 50px;
 			margin-top: 10px;
-			.col-6:first-child {
+			.col-lg-6:first-child {
 				padding: 50px;
 				p {
 					font-size: 3.75rem;
@@ -110,7 +124,7 @@ export default {
 				}
 				
 			}
-			.col-6:nth-child(2n) {
+			.col-lg-6:nth-child(2n) {
 				padding: 50px;
 				h6 {
 					margin: 25px 0;
@@ -144,6 +158,12 @@ export default {
 		}
 	}
 }
+a {
+						text-decoration: none;
+						color: white;
+						padding-left: 25px;
+						font-size: 1.5rem;
+					}
 
 
 
